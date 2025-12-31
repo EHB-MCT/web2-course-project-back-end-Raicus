197f7f214b60e0cd3c28b87071e0b977db48a732
Raicus <hamza.el.aisati@student.ehb.be>
2025-12-31 01:57:50 +0100

    made get route already for the team composition

--- server.js
+++ server.js
@@ -54,6 +54,32 @@ app.get("/maps", async (req, res) => {
 	}
 });
 
+app.get("/teams", async (req, res) => {
+	try {
+		const data = await db.collection("valorantData").findOne();
+		const teams = data.teams.map((team) => {
+			return {
+				composition_id: team.composition_id,
+				map_name: team.map_name,
+				agent_composition: team.agent_composition,
+			};
+		});
+		res.json(teams);
+	} catch (error) {
+		res.status(500).json({ error: "Database error", details: error.message });
+	}
+});
+
+// example (will be deleted later)
+app.get("/all", async (req, res) => {
+	try {
+		const users = await db.collection("valorantData").find().toArray(); // testing find() for filter later (very interesting)
+		res.json(users);
+	} catch (error) {
+		res.status(500).json({ error: "Database error" });
+	}
+});
+
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`);
 });
